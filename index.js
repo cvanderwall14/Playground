ec1a1d457e2f77cd0e56281756c461172a9778d2
Christian Vanderwall <cvanderwall14@gmail.com>
2014-12-30 10:15:24 -0800

    Add debug

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
  * @author Christian Vanderwall
  */
 
+import Debug from 'debug'
 import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
 
 // Master volume
-var amp = "=====".length*.1
+var amp = "======".length*.1
 
 export function dsp(t) {
+
   return ( sin(t,440) ) * amp;
+  
 }
