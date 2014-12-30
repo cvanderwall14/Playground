7a818aa1ae56f66172c3548f60095517f1165df7
Christian Vanderwall <cvanderwall14@gmail.com>
2014-12-30 09:32:08 -0800

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/**
+ * @name Playground
+ * @author Christian Vanderwall
+ */
+
+import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
+
+// Master volume
+var amp = "=====".length*.1
+
+export function dsp(t) {
+  return ( sin(t,440) ) * amp;
+}
